4cb39ee911dbf8e4c555ebae47c86f2f80914c21
kotaro1574 <shake1574@gmail.com>
2024-12-09 10:14:13 +0900

    add: `clsx` と `tailwind-merge` 追加

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
+import { Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
   return (
