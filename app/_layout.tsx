0b7ffc4330ac8d899737af041e82579b942b1000
kotaro1574 <shake1574@gmail.com>
2024-12-09 10:01:53 +0900

    create: ログイン画面作ってみた

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,8 +1,8 @@
-import { Stack } from "expo-router";
+import { Slot } from "expo-router";
 
 // Import your global CSS file
 import "../global.css";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Slot />;
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,37 +1,29 @@
-import { useState } from "react";
-import { Modal, Text, TouchableOpacity, View } from "react-native";
+import { Button, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function Index() {
-  const [isModalVisible, setIsModalVisible] = useState(false);
-
   return (
-    <View className="flex-1 items-center justify-center bg-gray-200">
-      <Text className="text-2xl font-bold mb-4">モーダルが開くだけです</Text>
-
-      {/* モーダルを開くボタン */}
-      <TouchableOpacity
-        onPress={() => setIsModalVisible(true)}
-        className="mt-8 bg-blue-400 px-6 py-2 rounded-lg"
-      >
-        <Text className="text-white text-lg">開く</Text>
-      </TouchableOpacity>
-
-      {/* モーダルの表示 */}
-      <Modal transparent={true} visible={isModalVisible} animationType="fade">
-        <View className="flex-1 justify-center items-center bg-black bg-opacity-100">
-          <View className="bg-white p-8 rounded-lg shadow-lg w-4/5 max-w-sm">
-            <Text className="text-xl font-bold mb-4">開きました</Text>
-            <View className="items-center">
-              <TouchableOpacity
-                onPress={() => setIsModalVisible(false)}
-                className="bg-blue-400 px-6 py-2 rounded-lg"
-              >
-                <Text className="text-white font-semibold">閉じる</Text>
-              </TouchableOpacity>
-            </View>
-          </View>
+    <View className="container max-w-[450px] py-[120px]">
+      <Text className="text-center text-[24px] font-bold text-[#c2b37f]">
+        ログイン
+      </Text>
+      <View className="mt-[24px] p-10">
+        <View>
+          <Text className="text-[14px] text-neutral-400">id</Text>
+          <TextInput className="mt-1 h-10 w-full rounded-[10px] border-[0.5px] border-[#A4A4A4] bg-white px-3 py-2 text-base text-black disabled:cursor-not-allowed disabled:opacity-50" />
         </View>
-      </Modal>
+        <View className="mt-4">
+          <Text className="text-[14px] text-neutral-400">パスワード</Text>
+          <TextInput className="mt-1 h-10 w-full rounded-[10px] border-[0.5px] border-[#A4A4A4] bg-white px-3 py-2 text-base text-black disabled:cursor-not-allowed disabled:opacity-50" />
+        </View>
+        <View className="mt-12">
+          <TouchableOpacity className="inline-flex items-center justify-center text-md transition-colors disabled:opacity-50 disabled:pointer-events-none bg-[#FFCA0E] shadow-shadow text-white hover:bg-[#FFCA0E]/80 h-10 py-2 px-4 rounded-[90px]">
+            <Text className="text-white">ログイン</Text>
+          </TouchableOpacity>
+          <TouchableOpacity className="mt-6 inline-flex items-center justify-center text-md transition-colors disabled:opacity-50 disabled:pointer-events-none bg-[#FFCA0E] shadow-shadow text-white hover:bg-[#FFCA0E]/80 h-10 py-2 px-4 rounded-[90px]">
+            <Text className="text-white">新規登録</Text>
+          </TouchableOpacity>
+        </View>
+      </View>
     </View>
   );
 }
