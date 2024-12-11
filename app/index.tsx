0993434013690d7ce4c0ec4d59b27abfb6af830e
kotaro1574 <shake1574@gmail.com>
2024-12-11 16:07:49 +0900

    update: inputの値を `react hook form` で管理

--- app/index.tsx
+++ app/index.tsx
@@ -1,8 +1,24 @@
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Text, View } from "react-native";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Controller, useForm } from "react-hook-form";
+
+const formSchema = z.object({
+  id: z.string().min(1, "idを入力してください"),
+  password: z.string(),
+});
 
 export default function Index() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      id: "",
+      password: "",
+    },
+  });
+
   return (
     <View className="container max-w-[450px] py-[120px] bg-white">
       <Text className="text-center text-[24px] font-bold text-[#c2b37f]">
@@ -11,11 +27,28 @@ export default function Index() {
       <View className="mt-[24px] p-10">
         <View>
           <Text className="text-[14px] text-neutral-400">id</Text>
-          <Input className="mt-1" />
+          <Controller
+            control={form.control}
+            name="id"
+            render={({ field: { value, onChange } }) => (
+              <Input className="mt-1" value={value} onChangeText={onChange} />
+            )}
+          />
         </View>
         <View className="mt-4">
           <Text className="text-[14px] text-neutral-400">パスワード</Text>
-          <Input className="mt-1" />
+          <Controller
+            control={form.control}
+            name="password"
+            render={({ field: { value, onChange } }) => (
+              <Input
+                className="mt-1"
+                value={value}
+                onChangeText={onChange}
+                secureTextEntry
+              />
+            )}
+          />
         </View>
         <View className="mt-12">
           <Button>ログイン</Button>
