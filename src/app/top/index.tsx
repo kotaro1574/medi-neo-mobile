5026525de1d87b289b8dc54b2ababe5e500d6498
kotaro1574 <shake1574@gmail.com>
2024-12-17 12:34:14 +0900

    update: 警告が出ていたパッケージを更新

--- src/app/top/index.tsx
+++ src/app/top/index.tsx
@@ -1,14 +1,9 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { supabase } from "@/lib/supabase/supabase";
 import { User } from "@supabase/supabase-js";
-import { Stack } from "expo-router";
-import { useState, useEffect } from "react";
-import {
-  View,
-  Text,
-  Alert,
-  SafeAreaView,
-  TouchableOpacity,
-} from "react-native";
+import { useEffect, useState } from "react";
+import { Alert, SafeAreaView, Text, View } from "react-native";
 
 export default function Top() {
   const [user, setUser] = useState<User | null>(null);
@@ -30,13 +25,25 @@ export default function Top() {
   };
 
   return (
-    <SafeAreaView style={{ flex: 1 }}>
-      <Stack.Screen options={{ headerShown: true, title: "Settings" }} />
-      <View style={{ padding: 16 }}>
-        <Text>{JSON.stringify(user, null, 2)}</Text>
-        <TouchableOpacity onPress={doLogout}>
-          <Text>LOGOUT</Text>
-        </TouchableOpacity>
+    <SafeAreaView>
+      <View className="min-h-screen bg-[#F5F5F5] px-4 pb-8 pt-[62px]">
+        <Text className="text-[20px] text-[#C2B37F]">アカウント情報</Text>
+        <View className="mt-4 space-y-4 rounded-2xl bg-white p-4">
+          <View>
+            <Text className="text-[14px] text-neutral-400">ユーザー名</Text>
+            <Input className="mt-1" value={user?.email} />
+          </View>
+          <View className="mt-4">
+            <Text className="text-[14px] text-neutral-400">所属施設</Text>
+            <Text className="mt-1">施設名</Text>
+          </View>
+        </View>
+        <Button className="mt-4" onPress={() => {}}>
+          更新
+        </Button>
+        <Button className="mt-4" onPress={doLogout} variant="destructive">
+          ログアウト
+        </Button>
       </View>
     </SafeAreaView>
   );
