8a06e09bdd5db2b3f72dd7e3358dee7bc105cb89
kotaro1574 <shake1574@gmail.com>
2024-12-20 11:30:45 +0900

    create: 施設を選択するselect作成

---
+++ src/feature/facilities/facilities-select.tsx
@@ -0,0 +1,45 @@
+import { Select } from "@/components/ui/select";
+import { supabase } from "@/lib/supabase/supabase";
+import { Tables } from "@/types/database.types";
+import { useEffect, useState } from "react";
+
+type Props = {
+  onValueChange: (value: string) => void;
+  defaultValue: string;
+  isError: boolean;
+};
+
+export function FacilitiesSelect({
+  onValueChange,
+  defaultValue,
+  isError,
+}: Props) {
+  const [facilities, setFacilities] = useState<Tables<"facilities">[]>([]);
+
+  useEffect(() => {
+    async function fetchFacilities() {
+      const { data, error } = await supabase.from("facilities").select("*");
+      if (error) {
+        console.error("Error fetching facilities", error);
+        return;
+      }
+
+      setFacilities(data);
+    }
+
+    fetchFacilities();
+  }, []);
+
+  return (
+    <Select
+      items={facilities.map((facility) => ({
+        label: facility.name_jp,
+        value: facility.id,
+      }))}
+      placeholder={{ label: "施設を選択", value: "" }}
+      value={defaultValue}
+      onValueChange={onValueChange}
+      className={isError ? "border-destructive" : ""}
+    />
+  );
+}
