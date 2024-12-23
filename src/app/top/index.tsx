db2f8a707d22025794784092db975910010eb840
kotaro1574 <shake1574@gmail.com>
2024-12-23 10:48:05 +0900

    update: 撮影した写真情報を取得

--- src/app/top/index.tsx
+++ src/app/top/index.tsx
@@ -1,11 +1,12 @@
 import { Button } from "@/components/ui/button";
 import { CameraType, CameraView, useCameraPermissions } from "expo-camera";
 import { useState } from "react";
-import { Text, View } from "react-native";
+import { SafeAreaView, Text, View } from "react-native";
 
 export default function Top() {
   const [facing, setFacing] = useState<CameraType>("back");
   const [permission, requestPermission] = useCameraPermissions();
+  const [camera, setCamera] = useState<CameraView | null>(null);
 
   if (!permission) {
     return <View />;
@@ -22,24 +23,39 @@ export default function Top() {
     );
   }
 
-  function toggleCameraFacing() {
+  const toggleCameraFacing = () => {
     setFacing((current) => (current === "back" ? "front" : "back"));
-  }
+  };
+
+  const takePicture = async () => {
+    if (camera) {
+      const photo = await camera.takePictureAsync();
+      console.log(photo);
+      // ここで撮影した写真を処理（保存、表示など）
+    }
+  };
 
   return (
-    <View className="h-screen bg-white p-4">
-      <CameraView
-        style={{
-          width: "100%",
-          height: "70%",
-          backgroundColor: "black",
-          borderRadius: 24,
-        }}
-        facing={facing}
-      />
-      <View className="mt-4">
-        <Button onPress={toggleCameraFacing}>Flip Camera</Button>
+    <SafeAreaView>
+      <View className="h-screen bg-white p-4">
+        <View className="h-[70%] overflow-hidden rounded-3xl">
+          <CameraView
+            style={{
+              width: "100%",
+              height: "100%",
+              backgroundColor: "black",
+            }}
+            facing={facing}
+            ref={setCamera}
+          />
+        </View>
+        <View className="mt-4">
+          <Button onPress={toggleCameraFacing}>Flip Camera</Button>
+        </View>
+        <View className="mt-4">
+          <Button onPress={takePicture}>Take Picture</Button>
+        </View>
       </View>
-    </View>
+    </SafeAreaView>
   );
 }
