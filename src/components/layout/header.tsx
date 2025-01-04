5a2032df4c99338674e40ad791a2760acae2ea02
kotaro1574 <shake1574@gmail.com>
2025-01-04 12:02:53 +0900

    update: icon表示したり、style調整したり

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -1,5 +1,6 @@
 import { Link } from "expo-router";
 import { Text, View } from "react-native";
+import { Icons } from "../ui/icon";
 
 type Props = {
   profileName: string;
@@ -8,12 +9,20 @@ type Props = {
 export function Header({ profileName }: Props) {
   return (
     <View className="fixed top-0 z-40 w-full max-w-3xl bg-white">
-      <View className="flex h-[54px] items-center justify-between space-x-4 px-4 py-3">
+      <View className="h-[54px] flex-row items-center justify-between space-x-4 px-4 py-3">
         <Link href="/">
-          <View className="flex items-center gap-0.5 text-[#a4a4a4]">
-            <Text>{profileName}</Text>
+          <View className="flex-row items-center gap-0.5 text-[#a4a4a4]">
+            <View>
+              <Icons.fillUser color="#a4a4a4" />
+            </View>
+            <View>
+              <Text>{profileName}</Text>
+            </View>
           </View>
         </Link>
+        <View>
+          <Icons.hamburger color="#a4a4a4" />
+        </View>
       </View>
     </View>
   );
